31fe01c2ae4f915c6ce8843774e3dae91dafb7e3
Kim JiHyun <123639754+zomeong@users.noreply.github.com>
2023-11-02 13:50:57 +0900

    Update 42839.cpp

--- 42839.cpp
+++ 42839.cpp
@@ -3,14 +3,25 @@
 #include <algorithm>
 #include <cstring>
 #include <vector>
-#include <queue>
-#define MAX 10
 #define FIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
 string numbers;
-int num[MAX];
+vector <int> num;
 vector <int> v;
+
+// 모든 조합 구하기
+void makeNums(int len) {
+	for (int j = 1; j <= len; j++) {
+		do {
+			int n = 0;
+			for (int i = 0; i < j; i++)
+				n += pow(10, j - i - 1) * num[i];		
+			v.push_back(n);
+
+		} while (next_permutation(num.begin(), num.end()));
+	}
+}
 
 // 소수인지 판별
 bool isPrime(int n) {
@@ -26,23 +37,21 @@ bool isPrime(int n) {
 int main() {
 	FIO;
 	int answer = 0;
-	int len = numbers.length();
 
 	cin >> numbers;
+	int len = numbers.length();
 
 	for (int i = 0; i < len; i++) 
-		num[i] = numbers[i] - '0';		// 문자열을 순회하며 숫자 하나씩 배열에 넣음
+		num.push_back(numbers[i] - '0');	// 문자열을 순회하며 숫자 하나씩 벡터에 넣음
 
-
-	// 모든 순열 구하기 
-	// next_permutation....
-
+	sort(num.begin(), num.end());
+	makeNums(len);						
 
 	sort(v.begin(), v.end());
 	v.erase(unique(v.begin(), v.end()), v.end());	// 중복 제거
 
 	for (int i = 0; i < v.size(); i++)
-		if (isPrime(v[i])) answer++;	// 소수인지 확인		
+		if (isPrime(v[i])) answer++;	// 소수인지 확인	
 
 	cout << answer;
 	
