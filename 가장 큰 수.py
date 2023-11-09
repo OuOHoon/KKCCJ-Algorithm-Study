be32622242a1035205783c57cd5f2e819592928b
OuOHoon <wls2579@gmail.com>
2023-11-09 12:04:27 +0900

    6회차 가장 큰 수

---
+++ 가장 큰 수.py
@@ -0,0 +1,10 @@
+# 숫자의 앞 자리수가 큰 순서대로 정렬한다
+# 문자를 여러번 반복해서 최소 4자릿수로 만들어서 비교한다
+# 정렬한 배열의 원소들을 문자열로 만든다
+
+def solution(numbers):
+    l = sorted(map(str, numbers), key=lambda x: x*4, reverse=True)
+    return str(int(''.join(l)))
+
+solution([6, 10, 2])
+solution([31, 312, 313, 318, 319]) #
