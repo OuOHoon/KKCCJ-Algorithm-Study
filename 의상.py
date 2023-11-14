7f51f087e10f4b9d78174dd0ab7031684a4c83e8
OuOHoon <wls2579@gmail.com>
2023-11-14 10:54:23 +0900

    9회차 의상

---
+++ 의상.py
@@ -0,0 +1,12 @@
+# 해시로 옷 종류별로 저장한다
+# 각 종류별로 입기+안입기로 곱하고, 아무것도 안입는 경우 1을 뺀다
+def solution(clothes):
+    answer = 1
+    d = {}
+    for _, category in clothes:
+        d[category] = d.get(category, 0) + 1
+
+    for category in d:
+        answer *= (d[category] + 1)
+
+    return answer - 1
--- 전화번호 목록.py
+++ 전화번호 목록.py
@@ -1,7 +1,7 @@
 # 모든 가능한 접두어를 해시테이블에 저장하고 비교하면 n*m. 범위가 작아서 가능
 
 def solution(phone_book):
-    d = {phone[:i]:True for phone in phone_book for i in range(1, len(phone))}
+    d = {phone[:i]: True for phone in phone_book for i in range(1, len(phone))}
     for phone in phone_book:
         if d.get(phone, False):
             return False
